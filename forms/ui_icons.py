1e96b0c88515eafa1fa32a74991eeff593b6612b
abdelhafidhousoufou <abdelhafidhou@live.fr>
2024-11-16 17:04:01 +0300

    forms

--- forms/ui_icons.py
+++ forms/ui_icons.py
@@ -14,110 +14,221 @@ class Icons:
         }
 
     def __QIcon(self, img: str) -> QIcon:
+        """
+        Création d'un QIcon simplifier à partir de la bibliothèque d'images
+        :param img: Nom de l'image
+        :return: un QIcon
+        """
         icon: QIcon = QIcon()
         icon.addFile(self.images.get(img), QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         return icon
 
     def __QPixmap(self, img: str) -> QPixmap:
+        """
+        Création d'un QPixmap simplifier à partir de la bibliothèque d'images
+        :param img: Nom de l'image
+        :return: un QPixmap
+        """
         return QPixmap(self.images.get(img))
 
     @property
     def entreprise_pixmap_icon(self) -> QPixmap:
+        """
+        Un pixmap de l'icon de l'entreprise
+        :return: un QPixmap
+        """
         return self.__QPixmap("MsCles")
 
     @property
     def entreprise_qicon(self) -> QIcon:
+        """
+        Un QIcon de l'icon de l'entreprise
+        :return: un QIcon
+        """
         return self.__QIcon("MsCles")
 
     @property
     def tableau_de_bord_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon de tableau de bord
+        :return: un QIcon
+        """
         return self.__QIcon("tableau-de-bord")
 
     @property
     def espace_de_travail_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon espace de travail
+        :return: un QIcon
+        """
         return self.__QIcon("les-conditions-de-travail")
 
     @property
     def gestion_facture_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon gestion facture
+        :return: un QIcon
+        """
         return self.__QIcon("facture-dachat")
 
     @property
     def gestion_inventaire_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon gestion des inventaires
+        :return: un QIcon
+        """
         return self.__QIcon("inventaire")
 
     @property
     def gestion_base_de_donnees_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon gestion de base de donnees
+        :return:
+        """
         return self.__QIcon("bases-de-donnees")
 
     def profil_pixmap(self, img: str = "patron") -> QPixmap:
+        """
+        Un pixmap de l'image profil
+        :param img: nom du profil
+        :return: un QPixmap
+        """
         return self.__QPixmap(img)
 
     @property
     def deconnexion_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon deconnexion
+        :return: un QIcon
+        """
         return self.__QIcon("sortir")
 
     @property
     def logo_connexion_pixmap(self) -> QPixmap:
+        """
+        Un pixmap de l'icon logo connexion
+        :return: un QPixmap
+        """
         return self.__QPixmap("connexion")
 
     @property
     def connexion_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon connexion
+        :return: un QIcon
+        """
         return self.__QIcon("login")
 
     @property
     def config_db_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon config bd
+        :return: un QIcon
+        """
         return self.__QIcon("configBD")
 
     @property
     def invite_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon invite
+        :return: un QIcon
+        """
         return self.__QIcon("invite")
 
     @property
     def retour_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon retour
+        :return: un QIcon
+        """
         return self.__QIcon("back")
 
     @property
     def logo_config_db_pixmap(self) -> QPixmap:
+        """
+        Un pixmap de l'icon logo config db
+        :return: un QPixmap
+        """
         return self.__QPixmap("configDB")
 
     @property
     def disquette_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon disquette de sauvegarde
+        :return: un QIcon
+        """
         return self.__QIcon("disquette")
 
     @property
     def export_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon export
+        :return: un QIcon
+        """
         return self.__QIcon("partager")
 
     @property
     def calendrier_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon calendrier
+        :return: un QIcon
+        """
         return self.__QIcon("calendrier")
 
     @property
     def activites_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon activites
+        :return: un QIcon
+        """
         return self.__QIcon("besoins")
 
     @property
     def action_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon action
+        :return: un QIcon
+        """
         return self.__QIcon("lecriture")
 
     @property
     def budget_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon budget
+        :return: un QIcon
+        """
         return self.__QIcon("croissance")
 
     @property
     def plus_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon plus
+        :return: un QIcon
+        """
         return self.__QIcon("plus")
 
     @property
     def ajouter_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon ajouter
+        :return: un QIcon
+        """
         return self.__QIcon("ajouter")
 
     @property
     def mise_a_jour_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon mise a jour
+        :return: un QIcon
+        """
         return self.__QIcon("maj")
 
     @property
     def supprimer_icon(self) -> QIcon:
+        """
+        Un QIcon de l'icon moins
+        :return:
+        """
         return self.__QIcon("moins")
 
     @property
