989ded6fe00e89cc61eca1aec134ebc787dde948
abdelhafidhousoufou <abdelhafidhou@live.fr>
2025-01-21 22:02:52 +0300

    theme

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ class DevisCreator(PostgreSQLDatabase, Formulaire, Layout, ActivityInsert):
         self.maindialog._b_invoice_export.clicked.connect(self.ModelFacture)
         self.maindialog._b_invoice_add_cart.clicked.connect(self.addToCart)
 
-
     def mkOutputFolder(self):
         self.outputfolder = Path.home() / "Documents" / "Sorties DevisCreator"
         self.outputfolder.mkdir(exist_ok=True, parents=True)
